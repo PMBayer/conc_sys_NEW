a8179eebac26b9ae406db14b59652ef1a5190eaa
Pascal Bayer <pascal.bayer1999@gmail.com>
2022-11-19 14:34:41 +0100

    updated SiL; Rule for steering left seems to be broken

--- src/ROBOT/Applications/SiLTestAPP.java
+++ src/ROBOT/Applications/SiLTestAPP.java
@@ -3,7 +3,7 @@ package ROBOT.Applications;
 import ROBOT.Logic.Controller;
 import ROBOT.Tests.SiLTest;
 
-public class SiLSiL {
+public class SiLTestAPP {
     public static void main(String[] args) throws Exception {
         SiLTest test = new SiLTest();
         Controller controller = new Controller(test, test);
